87ab36d044bafcad0dea3bb9655ca4abf4e314c7
minyi <minyi@mobvoi.com>
2017-02-13 20:27:56 +0800

    update

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 const passport = require('passport');
 var session = require('express-session');
 var mongoose = require('mongoose');
+var compression = require('compression');
 
 
 var db = mongoose.connect('mongodb://localhost/personalBlog');
@@ -34,6 +35,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
+app.use(compression());
 app.use(favicon(path.join(__dirname, 'public', '/img/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
